5db95dffb56d8a4b8e0f22c713edd6a115f25e40
Drata <adescudero93@gmail.com>
2017-12-05 00:26:38 +0100

    First Commit

---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour {
+
+    [System.Serializable]
+    public struct SistemaTiempo {
+        public ParticleSystem ps;
+        public float tiempo;
+    }
+
+    public SistemaTiempo[] sistemas;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Emite());	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Emite()
+    {
+        foreach (SistemaTiempo st in sistemas)
+        {
+            st.ps.Play();
+            yield return new WaitForSeconds(st.tiempo);
+            st.ps.Stop();
+        }
+        
+    }
+}
